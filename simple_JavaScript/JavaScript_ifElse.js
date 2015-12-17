a42003162a0c45665b9e86282be4b614b3c15204
Dan Billmann <billmannd@gmail.com>
2015-12-17 00:37:24 -0500

    Updated ifElse.js

--- simple_JavaScript/JavaScript_ifElse.js
+++ simple_JavaScript/JavaScript_ifElse.js
@@ -1,3 +1,22 @@
+/*
+// Booleans are a simple form of Algebra where every value is either TRUE or FALSE.
+// Here's how to declare a boolean:
+/*
+var Boolean = TRUE;
+var not_a_Boolean = FALSE;
+*/
+// Booleans are used as a condition against which other things are tested.
+
+// If/Else functions test if a condition is met and then takes a course of action based on that condition.
+// For example:
+
+//var go_bearcats = 2015;
+
+
+// If you are trying to make a condition equal to something, use "==". If you want it to not equal something, use "!="
+// Other conditions include ">, <, >=, <="
+// Here are a few examples:
+
 var age = 21;
 function ifFunction() {
   if (age < 65) {
