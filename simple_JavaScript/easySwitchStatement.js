2f936da53777b706eb06fa1a470fb9f27b623ea9
mhansen2710 <mhansen2710@gmail.com>
2015-12-16 22:05:15 -0500

    Create easySwitchStatement.js

---
+++ simple_JavaScript/easySwitchStatement.js
@@ -0,0 +1,23 @@
+switch(new Date().getDay()) {
+    case 0:
+        alert("Sunday Funday!")
+        break;
+    case 1:
+        alert("Monday's are rough...")
+        break;
+   	case 2:
+    	alert("Tuesday's are almost as bad as Mondays...");
+        break;
+    case 3:
+    	alert("Hump day!");
+        break;
+    case 4:
+    	alert("Almost the weekend!");
+        break;
+    case 5:
+    	alert("TGIF!");
+        break;
+	case 6:
+    	alert("Happy Saturday!");
+    break;
+}
