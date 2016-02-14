134d7611368d91325ab13a95b9d619bab98c0449
mhansen2710 <mhansen2710@gmail.com>
2016-02-14 10:36:16 -0500

    ChangedtoMatchPresentation
    Updated the functions and HTML so that it is actually what we are
    presenting to them

--- Sample_HTML/arrays.js
+++ Sample_HTML/arrays.js
@@ -1,48 +1,28 @@
 //array of datatypes
-var dataTypes = ["string", "char", "integer", "float", "boolean", "array"];
+var toDoList = ["Wake Up", "Brush Teeth", "Leave House"];
 
-//function to add things to datatype array
-/*function addToArray(){
-  //prompting users to add new info to the array and saving it to a variable
-  var newInfo = prompt("Tell me another data type: ");
-  //adding said variable into the array
-  dataTypes.push(newInfo);
-  //prints new array
-  printArray();
-}
-
-function printArray(){
-  var arrayString = ""
-
-    //loops through the entire array
-    for(i = 0; i < dataTypes.length; i++){
-      //alerts each position in array NEEDS TO CHANGE
-      arrayString = arrayString + dataTypes[i] + ", ";
-    }
-    alert(arrayString);
-} */
-
-
-
-function addToArray2(){
+function addToArray(){
   //prompting users to add new info to the array and saving it to a variable
   var newInfo = document.getElementById('inputs').value;
   //adding said variable into the array
-  dataTypes.push(newInfo);
-  //prints new array
-  printArray2();
-
+  toDoList.push(newInfo);
 }
 
-function printArray2(){
+function printArray(){
+    //clearing the div
+    document.getElementById('outputs').innerHTML = "";
     //loops through the entire array
-    for(i = 0; i < dataTypes.length; i++){
+    for(i = 0; i < toDoList.length; i++){
       //alerts each position in array NEEDS TO CHANGE
       var div = document.getElementById('outputs');
-      div.innerHTML = div.innerHTML + '<br/>' + dataTypes[i];
+      //adding the information to the div
+      div.innerHTML = div.innerHTML + '<br/>' + i + ". " + toDoList[i];
     }
 }
 
-function clearDiv() {
-  document.getElementById('outputs').innerHTML = "";
+function removeFromDiv(){
+  //ask the user what they want to delete
+  var deleteItem = prompt("Enter number of item you want to remove: ");
+  //removesfrom the array
+  toDoList.splice(deleteItem , 1);
 }
